09231a09c044bec180a6e7c61d94f86db3d8ff68
Jesse Xu <jesse0628@gmail.com>
2017-12-06 17:28:30 +1300

    Remove ignored files again

--- Java/src/KEmptySlots_683.java
+++
@@ -1,7 +0,0 @@
-
-public class KEmptySlots_683 {
-    public int kEmptySlots(int[] flowers, int k) {
-// Not Solved
-        return -1;
-    }
-}
--- Java/src/Solution.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-
-    public static void main(String[] args) {
-        NextClosestTime_681 s = new NextClosestTime_681();
-        String r = s.nextClosestTime("23:59");
-        System.out.print(r);
-    }
-
-}
