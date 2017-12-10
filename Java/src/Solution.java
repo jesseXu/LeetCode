f1eb529a36fcecde494d104a8cfb400bb9eaefc8
Jesse Xu <jesse0628@gmail.com>
2017-12-11 10:37:54 +1300

    fix

--- Java/src/Solution.java
+++ Java/src/Solution.java
@@ -4,11 +4,7 @@ public class Solution {
 //        BombEnemy_361 s = new BombEnemy_361();
 //        char[][] a = {{'0','E','0','0'},{'E','0','W','E'},{'0','E','0','0'}};
 //        int r = s.maxKilledEnemies(a);
-
-        int[] array = {235,140,4};
-        UTF8Validation_393 s = new UTF8Validation_393();
-        boolean r = s.validUtf8(array);
-        System.out.print(r);
+        
     }
 
 }
